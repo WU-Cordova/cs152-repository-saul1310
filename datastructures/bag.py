05124956129ddc281ac46c71c77ac8536d583bca
Saul Ifshin <97627272+saul1310@users.noreply.github.com>
2025-01-27 11:48:43 -0800

    test

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -1,27 +1,28 @@
-from typing import Iterable, Optional
+from typing import Dict, Iterable
 from datastructures.ibag import IBag, T
 
 class Bag(IBag[T]):
-    def __init__(self, *items: Optional[Iterable[T]]) -> None:
-        raise NotImplementedError("__init__ method not implemented")
+    def __init__(self, *items: T) -> None:
+        self.__bag: Dict[T, int] = {}
+        for item in items: self.add(item)
 
     def add(self, item: T) -> None:
-        raise NotImplementedError("add method not implemented")
-
+        if item is None: raise TypeError("Item cannot be None")
+        
+        self.__bag[item] = self.__bag.get(item, 0) + 1
+        
     def remove(self, item: T) -> None:
-        raise NotImplementedError("remove method not implemented")
+        if item not in self.__bag: raise ValueError(f"{item} not in bag")
+        
+        self.__bag[item] -= 1
+        if self.__bag[item] == 0: del self.__bag[item]
 
-    def count(self, item: T) -> int:
-        raise NotImplementedError("count method not implemented")
+    def count(self, item: T) -> int: return self.__bag.get(item, 0)
 
-    def __len__(self) -> int:
-        raise NotImplementedError("__len__ method not implemented")
+    def __len__(self) -> int: return sum(self.__bag.values())
 
-    def distinct_items(self) -> Iterable[T]:
-        raise NotImplementedError("distinct_items method not implemented")
+    def distinct_items(self) -> Iterable[T]: return self.__bag.keys()
 
-    def __contains__(self, item) -> bool:
-        raise NotImplementedError("__contains__ method not implemented")
+    def __contains__(self, item) -> bool: return item in self.__bag
 
-    def clear(self) -> None:
-        raise NotImplementedError("clear method not implemented")
+    def clear(self) -> None: self.__bag.clear()
